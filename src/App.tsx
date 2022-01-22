524bfd592ee8821d6c9a585e7f52cc20701818eb
Wanderson Alves <wanderson.alves.rodrigues@gmail.com>
2022-01-22 11:54:47 -0300

    feat: adicionada theme light

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
-import React from "react";
 import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "@emotion/react";
+
+import {LightTheme} from './shared/themes'
 
 import { AppRoutes } from "./routes";
 
 export const App = () => {
   return (
-    <BrowserRouter>
-      <AppRoutes />
-    </BrowserRouter>
+    <ThemeProvider theme={LightTheme}>
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
+    </ThemeProvider>
   );
 };
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,7 +4,14 @@ import { Button } from "@mui/material";
 export const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/pagina-inicial" element={<Button>Teste</Button>} />
+      <Route
+        path="/pagina-inicial"
+        element={
+          <Button variant="contained" color="primary">
+            Teste
+          </Button>
+        }
+      />
       <Route path="*" element={<Navigate to="/pagina-inicial" />} />
     </Routes>
   );
