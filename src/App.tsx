f89adf1272f27e4980e0735b50735334fc375c52
Wanderson Alves <wanderson.alves.rodrigues@gmail.com>
2022-01-29 13:01:05 -0300

    feat: criação menu lateral

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,15 @@ import { AppThemeProvider } from './shared/contexts';
 
 import { AppRoutes } from './routes';
 
+import { MenuLateral } from './shared/components';
 
 export const App = () => {
   return (
     <AppThemeProvider>
       <BrowserRouter>
-        <AppRoutes />
+        <MenuLateral>
+          <AppRoutes />
+        </MenuLateral>
       </BrowserRouter>
     </AppThemeProvider>
   );
---
+++ src/shared/components/menu-lateral/MenuLateral.tsx
@@ -0,0 +1,42 @@
+import { Home } from '@mui/icons-material';
+import { Avatar, Divider, Drawer, List, ListItemButton, ListItemIcon, ListItemText, useTheme } from '@mui/material';
+import { Box } from '@mui/system';
+
+
+export const MenuLateral: React.FC = ({ children }) => {
+  const theme = useTheme();
+
+  return (
+    <>
+      <Drawer variant='permanent'>
+        <Box width={theme.spacing(28)} height="100%" display="flex" flexDirection="column">
+
+          <Box width="100%" height={theme.spacing(20)} display="flex" alignItems="center" justifyContent="center">
+            <Avatar
+              sx={{ height: theme.spacing(12), width: theme.spacing(12) }}
+              src="https://avatars.githubusercontent.com/u/3501534?v=4"
+            />
+          </Box>
+
+          <Divider />
+
+          <Box flex={1}>
+            <List component="nav">
+              <ListItemButton>
+                <ListItemIcon>
+                  <Home/>
+                </ListItemIcon>
+                <ListItemText primary="Página inicial" />
+              </ListItemButton>
+            </List>
+          </Box>
+
+        </Box>
+      </Drawer>
+
+      <Box height="100vh" marginLeft={theme.spacing(28)}>
+        {children}
+      </Box>
+    </>
+  );
+};
