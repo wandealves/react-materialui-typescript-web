cf2cd1a8ae6d500048480fe2e4f88b526f8bb7e9
Wanderson Alves <wanderson.alves.rodrigues@gmail.com>
2022-03-05 12:27:27 -0300

    feat: adição skeleton

--- src/shared/components/ferramentas-de-detalhe/FerramentasDeDetalhes.tsx
+++ src/shared/components/ferramentas-de-detalhe/FerramentasDeDetalhes.tsx
@@ -1,25 +1,30 @@
-import { Box, Button, Divider, Paper,useTheme } from '@mui/material';
-import { FaPlus,FaSave,FaTrash,FaArrowLeft } from 'react-icons/fa';
+import { Box, Button, Divider, Paper, Skeleton, useTheme } from '@mui/material';
+import { FaPlus, FaSave, FaTrash, FaArrowLeft } from 'react-icons/fa';
 
 interface IFerramentasDeDetalheProps {
-textoBotaoNovo?:string;
+  textoBotaoNovo?: string;
 
-mostrarBotaoNovo?:boolean;
-mostrarBotaoVoltar?:boolean;
-mostrarBotaoApagar?:boolean;
-mostrarBotaoSalvar?:boolean;
-mostrarBotaoSalvarEFechar?:boolean;
+  mostrarBotaoNovo?: boolean;
+  mostrarBotaoVoltar?: boolean;
+  mostrarBotaoApagar?: boolean;
+  mostrarBotaoSalvar?: boolean;
+  mostrarBotaoSalvarEFechar?: boolean;
 
-aoClicarEmNovo?: () => void;
-aoClicarEmVoltar?: () => void;
-aoClicarEmApagar?: () => void;
-aoClicarEmSalvar?: () => void;
-aoClicarEmSalvarEFechar?: () => void;
+  mostrarBotaoNovoCarregando?: boolean;
+  mostrarBotaoVoltarCarregando?: boolean;
+  mostrarBotaoApagarCarregando?: boolean;
+  mostrarBotaoSalvarCarregando?: boolean;
+  mostrarBotaoSalvarEFecharCarregando?: boolean;
 
+  aoClicarEmNovo?: () => void;
+  aoClicarEmVoltar?: () => void;
+  aoClicarEmApagar?: () => void;
+  aoClicarEmSalvar?: () => void;
+  aoClicarEmSalvarEFechar?: () => void;
 }
 
-export const FerramentasDeDetalhe:React.FC<IFerramentasDeDetalheProps> = ({
-  textoBotaoNovo='Novo',
+export const FerramentasDeDetalhe: React.FC<IFerramentasDeDetalheProps> = ({
+  textoBotaoNovo = 'Novo',
 
   mostrarBotaoNovo = true,
   mostrarBotaoVoltar = true,
@@ -27,58 +32,70 @@ export const FerramentasDeDetalhe:React.FC<IFerramentasDeDetalheProps> = ({
   mostrarBotaoSalvar = true,
   mostrarBotaoSalvarEFechar = false,
 
+  mostrarBotaoNovoCarregando = false,
+  mostrarBotaoVoltarCarregando = false,
+  mostrarBotaoApagarCarregando = false,
+  mostrarBotaoSalvarCarregando = false,
+  mostrarBotaoSalvarEFecharCarregando = false,
+
   aoClicarEmNovo,
   aoClicarEmVoltar,
   aoClicarEmApagar,
   aoClicarEmSalvar,
-  aoClicarEmSalvarEFechar,
-}) =>{
+  aoClicarEmSalvarEFechar
+}) => {
   const theme = useTheme();
-  return (<Box
-    height={theme.spacing(5)}
-    marginX={1}
-    padding={1}
-    paddingX={2}
-    display="flex"
-    gap={1}
-    alignItems="center"
-    component={Paper}
-  >
-    {
-      mostrarBotaoSalvar && (    <Button
-        variant="contained"
-        color="primary"
-        disableElevation
-        onClick={aoClicarEmSalvar}
-        startIcon={<FaSave />}
-      >
-             Salvar
-      </Button>)
-    }
-    {
-      mostrarBotaoSalvarEFechar && (    <Button
-        variant="outlined"
-        color="primary"
-        disableElevation
-        onClick={aoClicarEmSalvarEFechar}
-        startIcon={<FaSave />}
-      >
-         Salvar e voltar
-      </Button>)
-    }
-    {
-      mostrarBotaoApagar && (    <Button
-        variant="outlined"
-        color="primary"
-        disableElevation
-        onClick={aoClicarEmApagar}
-        startIcon={<FaTrash />}
-      >
-             Apagar
-      </Button>)
-    }
-    {
-      mostrarBotaoNovo && (
+  return (
+    <Box
+      height={theme.spacing(5)}
+      marginX={1}
+      padding={1}
+      paddingX={2}
+      display="flex"
+      gap={1}
+      alignItems="center"
+      component={Paper}
+    >
+      {mostrarBotaoSalvar && !mostrarBotaoSalvarCarregando && (
+        <Button
+          variant="contained"
+          color="primary"
+          disableElevation
+          onClick={aoClicarEmSalvar}
+          startIcon={<FaSave />}
+        >
+          Salvar
+        </Button>
+      )}
+      {mostrarBotaoSalvarCarregando && <Skeleton width={110} height={60} />}
+      {mostrarBotaoSalvarEFechar && !mostrarBotaoSalvarEFecharCarregando && (
+        <Button
+          variant="outlined"
+          color="primary"
+          disableElevation
+          onClick={aoClicarEmSalvarEFechar}
+          startIcon={<FaSave />}
+        >
+          Salvar e voltar
+        </Button>
+      )}
+      {mostrarBotaoSalvarEFecharCarregando && (
+        <Skeleton width={180} height={60} />
+      )}
+
+      {mostrarBotaoApagar && !mostrarBotaoApagarCarregando && (
+        <Button
+          variant="outlined"
+          color="primary"
+          disableElevation
+          onClick={aoClicarEmApagar}
+          startIcon={<FaTrash />}
+        >
+          Apagar
+        </Button>
+      )}
+      {mostrarBotaoApagarCarregando && <Skeleton width={110} height={60} />}
+      {mostrarBotaoNovo && !mostrarBotaoNovoCarregando && (
         <Button
           variant="outlined"
           color="primary"
@@ -88,23 +105,23 @@ export const FerramentasDeDetalhe:React.FC<IFerramentasDeDetalheProps> = ({
         >
           {textoBotaoNovo}
         </Button>
-      )
-    }
-
-    {
-      mostrarBotaoVoltar && (<>
-        <Divider variant='middle' orientation='vertical'/>
-        <Button
-          variant="outlined"
-          color="primary"
-          disableElevation
-          onClick={aoClicarEmVoltar}
-          startIcon={<FaArrowLeft />}
-        >
-           Voltar
-        </Button>
-      </>)
-    }
-
-  </Box>);
+      )}
+      {mostrarBotaoNovoCarregando && <Skeleton width={110} height={60} />}
+      {mostrarBotaoVoltar && !mostrarBotaoVoltarCarregando && (
+        <>
+          <Divider variant="middle" orientation="vertical" />
+          <Button
+            variant="outlined"
+            color="primary"
+            disableElevation
+            onClick={aoClicarEmVoltar}
+            startIcon={<FaArrowLeft />}
+          >
+            Voltar
+          </Button>
+        </>
+      )}
+      {mostrarBotaoVoltarCarregando && <Skeleton width={110} height={60} />}
+    </Box>
+  );
 };
