a50964be7e1895474903516ea1f117f3be079dbf
Wanderson Alves <wanderson.alves.rodrigues@gmail.com>
2022-04-17 11:11:45 -0300

    feat: criação hook

--- src/pages/pessoas/ListagemDePessoas.tsx
+++ src/pages/pessoas/ListagemDePessoas.tsx
@@ -1,28 +1,32 @@
 import React, { useEffect, useMemo } from 'react';
 import { useSearchParams } from 'react-router-dom';
 
+import {useDebounce} from '../../shared/hooks';
+
 import { FerramentasDaListagem } from '../../shared/components';
 import { LayoutBaseDePagina } from '../../shared/layouts';
 import { PessoasService } from '../../shared/services/api/pessoas/PessoasService';
 
-// import { Container } from './styles';
-
 export const ListagemDePessoas: React.FC = () => {
   const [searchParams, setSearchParams] = useSearchParams();
+  const {debounce} = useDebounce(3000);
 
   const busca = useMemo(() => {
     return searchParams.get('busca') || '';
   }, [searchParams]);
 
   useEffect(()=>{
-    PessoasService.getAll(1,busca).then((result)=>{
-      if(result instanceof Error){
-        alert(result.message);
-        return;
-      }
-
-      console.log('result',result);
+    debounce(()=>{
+      PessoasService.getAll(1,busca).then((result)=>{
+        if(result instanceof Error){
+          alert(result.message);
+          return;
+        }
+  
+        console.log('result',result);
+      });
     });
+
   },[busca]);
 
   return (
---
+++ src/shared/hooks/UseDebounce.ts
@@ -0,0 +1,25 @@
+import { useCallback, useRef } from 'react';
+export const useDebounce = (delay = 300, notDelayFirstTime = true) => {
+  const debouncing = useRef<NodeJS.Timeout>();
+  const isFirstTime = useRef(notDelayFirstTime);
+
+  const debounce = useCallback(
+    (func: () => void) => {
+      if (isFirstTime.current) {
+        isFirstTime.current = false;
+        func();
+      } else {
+        if (debouncing.current) {
+          clearTimeout(debouncing.current);
+        }
+
+        debouncing.current = setTimeout(() => {
+          func();
+        }, delay);
+      }
+    },
+    [delay]
+  );
+
+  return { debounce };
+};
