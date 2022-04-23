0c03a2355b8a4577fb242dfbb3e85ff150c982af
Wanderson Alves <wanderson.alves.rodrigues@gmail.com>
2022-04-23 11:08:21 -0300

    feat: criação paginação

--- src/pages/pessoas/ListagemDePessoas.tsx
+++ src/pages/pessoas/ListagemDePessoas.tsx
@@ -9,7 +9,8 @@ import {
   TableCell,
   Paper,
   TableFooter,
-  LinearProgress
+  LinearProgress,
+  Pagination
 } from '@mui/material';
 
 import { useDebounce } from '../../shared/hooks';
@@ -35,11 +36,15 @@ export const ListagemDePessoas: React.FC = () => {
     return searchParams.get('busca') || '';
   }, [searchParams]);
 
+  const pagina = useMemo(() => {
+    return Number(searchParams.get('pagina') || '1');
+  }, [searchParams]);
+
   useEffect(() => {
     setIsLoading(true);
 
     debounce(() => {
-      PessoasService.getAll(1, busca).then(result => {
+      PessoasService.getAll(pagina, busca).then(result => {
         setIsLoading(false);
 
         if (result instanceof Error) {
@@ -52,7 +57,7 @@ export const ListagemDePessoas: React.FC = () => {
         setTotalCount(result.totalCount);
       });
     });
-  }, [busca]);
+  }, [busca, pagina]);
 
   return (
     <LayoutBaseDePagina
@@ -63,7 +68,7 @@ export const ListagemDePessoas: React.FC = () => {
           mostarInputBusca
           textoDaBusca={busca}
           aoMudarTextoDeBusca={texto =>
-            setSearchParams({ busca: texto }, { replace: true })
+            setSearchParams({ busca: texto, pagina: '1' }, { replace: true })
           }
         />
       }
@@ -98,6 +103,22 @@ export const ListagemDePessoas: React.FC = () => {
               <TableRow>
                 <TableCell colSpan={3}>
                   <LinearProgress variant="indeterminate" />
+                </TableCell>
+              </TableRow>
+            )}
+            {totalCount > 0 && totalCount > Environment.LIMITE_DE_LINHAS && (
+              <TableRow>
+                <TableCell colSpan={3}>
+                  <Pagination
+                    page={pagina}
+                    count={Math.ceil(totalCount / Environment.LIMITE_DE_LINHAS)}
+                    onChange={(_, newPage) =>
+                      setSearchParams(
+                        { busca, pagina: newPage.toString() },
+                        { replace: true }
+                      )
+                    }
+                  />
                 </TableCell>
               </TableRow>
             )}
